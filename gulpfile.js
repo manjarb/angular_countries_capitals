f6dad9138557a0a38366c9d073f903ecae9a1baf
Varis Darasirikul <manjarb4@gmail.com>
2015-09-17 01:50:10 +0700

    geoapp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,28 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var usemin = require('gulp-usemin');
+var uglify = require('gulp-uglify');
+var minifyHtml = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
+var rev = require('gulp-rev');
+
+
 gulp.task('connect', function() {
   connect.server();
 });
+
+gulp.task('copy-html-files', function() {
+  gulp.src(['./app/**/*.html', './app/home.html', '!./app/index.html'], {base: './app'})
+  .pipe(gulp.dest('build/'));
+});
+
+gulp.task('usemin', function() {
+  gulp.src('./app/index.html')
+    .pipe(usemin({
+      css: [minifyCss(), 'concat', rev()],
+      js: [uglify(), rev()]
+    }))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('build', ['copy-html-files', 'usemin']);
